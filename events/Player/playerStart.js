570098ec8ed64c0edcc8e333dfd0cb2a8d4df263
KINGBOTs1 <ds767365@gmail.com>
2024-08-04 19:58:24 +0800

    Update playerStart.js

--- events/Player/playerStart.js
+++ events/Player/playerStart.js
@@ -15,77 +15,67 @@ module.exports = async (queue, track) => {
 
   const channel = queue.metadata.channel;
 
-  try {
-    // Menghapus semua pesan di channel
-    const messages = await channel.messages.fetch({
-      limit: 100
+  // Clear all messages in the channel
+  await channel.bulkDelete(100);
+
+  const embed = new EmbedBuilder()
+    .setAuthor({
+      name: await Translate(
+        `Started playing ${track.title} in ${queue.channel.name} 🎧`
+      ),
+      iconURL: track.thumbnail,
+    })
+    .setColor("#2f3136")
+    .setThumbnail(track.thumbnail)
+    .addFields({
+      name: "Title",
+      value: track.title,
+      inline: true
+    }, {
+      name: "Channel",
+      value: queue.channel.name,
+      inline: true
+    })
+    .setFooter({
+      text: "Enjoy your music!",
+      iconURL: track.thumbnail,
     });
-    await channel.bulkDelete(messages);
 
-    // Membuat embed
-    const embed = new EmbedBuilder()
-      .setAuthor({
-        name: await Translate(
-          `Started playing ${track.title} in ${queue.channel.name} 🎧`
-        ),
-        iconURL: track.thumbnail,
-      })
-      .setColor("#2f3136")
-      .setThumbnail(track.thumbnail)
-      .addFields({
-        name: "Title",
-        value: track.title,
-        inline: true
-      }, {
-        name: "Channel",
-        value: queue.channel.name,
-        inline: true
-      })
-      .setFooter({
-        text: "Enjoy your music!",
-        iconURL: track.thumbnail,
-      });
+  const back = new ButtonBuilder()
+    .setLabel(EmojiState ? emojis.back : 'Back')
+    .setCustomId('back')
+    .setStyle('Primary');
 
-    // Membuat tombol
-    const back = new ButtonBuilder()
-      .setLabel(EmojiState ? emojis.back : 'Back')
-      .setCustomId('back')
-      .setStyle('Primary');
+  const skip = new ButtonBuilder()
+    .setLabel(EmojiState ? emojis.skip : 'Skip')
+    .setCustomId('skip')
+    .setStyle('Primary');
 
-    const skip = new ButtonBuilder()
-      .setLabel(EmojiState ? emojis.skip : 'Skip')
-      .setCustomId('skip')
-      .setStyle('Primary');
+  const resumepause = new ButtonBuilder()
+    .setLabel(EmojiState ? emojis.ResumePause : 'Resume & Pause')
+    .setCustomId('resume&pause')
+    .setStyle('Danger');
 
-    const resumepause = new ButtonBuilder()
-      .setLabel(EmojiState ? emojis.ResumePause : 'Resume & Pause')
-      .setCustomId('resume&pause')
-      .setStyle('Danger');
+  const loop = new ButtonBuilder()
+    .setLabel(EmojiState ? emojis.loop : 'Loop')
+    .setCustomId('loop')
+    .setStyle('Danger');
 
-    const loop = new ButtonBuilder()
-      .setLabel(EmojiState ? emojis.loop : 'Loop')
-      .setCustomId('loop')
-      .setStyle('Danger');
+  const lyrics = new ButtonBuilder()
+    .setLabel(EmojiState ? emojis.lirik : 'Lyrics')
+    .setCustomId("lyrics")
+    .setStyle("Secondary");
 
-    const lyrics = new ButtonBuilder()
-      .setLabel(EmojiState ? emojis.lirik : 'Lyrics')
-      .setCustomId("lyrics")
-      .setStyle("Secondary");
+  const row1 = new ActionRowBuilder().addComponents(
+    back,
+    loop,
+    resumepause,
+    skip,
+    lyrics
+  );
 
-    // Mengirim embed dan tombol
-    const row1 = new ActionRowBuilder().addComponents(
-      back,
-      loop,
-      resumepause,
-      skip,
-      lyrics
-    );
-
-    await channel.send({
-      embeds: [embed],
-      components: [row1]
-    });
-  } catch (error) {
-    console.error('Failed to clear messages or send embed:', error);
-  }
+  channel.send({
+    embeds: [embed],
+    components: [row1]
+  });
 };
